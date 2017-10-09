33ae71af51de70a0bed964dc486605414ce84cdd
Karen <berntsenkaren@hotmail.com>
2017-10-09 15:41:27 +0200

    Added Mens erger je niet

--- src/main/java/nl/MensErgerJeNiet/mensergerjeniet/MensergerjenietApplication.java
+++ src/main/java/nl/MensErgerJeNiet/mensergerjeniet/MensergerjenietApplication.java
@@ -3,10 +3,13 @@ package nl.MensErgerJeNiet.mensergerjeniet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import nl.MensErgerJeNiet.mensergerjeniet.game.controller.StartMensErgerJeNiet;
+
 @SpringBootApplication
 public class MensergerjenietApplication {
 
 	public static void main(String[] args) {
 	System.out.println("test");	SpringApplication.run(MensergerjenietApplication.class, args);
+	StartMensErgerJeNiet.main(null);
 	}
 }
