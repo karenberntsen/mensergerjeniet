6d7d193400438c6a48870eaf802da4e4d0182a41
Karen <berntsenkaren@hotmail.com>
2017-10-09 14:06:50 +0200

    first commit

---
+++ src/main/java/nl/MensErgerJeNiet/mensergerjeniet/MensergerjenietApplication.java
@@ -0,0 +1,12 @@
+package nl.MensErgerJeNiet.mensergerjeniet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensergerjenietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensergerjenietApplication.class, args);
+	}
+}
