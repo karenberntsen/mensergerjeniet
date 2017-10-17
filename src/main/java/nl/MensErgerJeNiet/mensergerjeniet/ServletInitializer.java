6e065beb1204e57163286e422ee019b127584136
yasper <yasper_katgert@hotmail.com>
2017-10-17 11:12:46 +0200

    servletinit toegevoegd voor war

---
+++ src/main/java/nl/MensErgerJeNiet/mensergerjeniet/ServletInitializer.java
@@ -0,0 +1,13 @@
+package nl.MensErgerJeNiet.mensergerjeniet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MensergerjenietApplication.class);
+	}
+
+}
